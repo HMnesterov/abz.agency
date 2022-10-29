37d639c463537c7d70b77e6b364291b42d2c3d14
BenitoSwaggolini <profie_player@mail.ru>
2022-10-29 15:52:44 +0500

    init

--- config/tree/models.py
+++ config/tree/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Job(models.Model):
+    name = models.CharField(50)
+    text = models.TextField(1000, blank=True)
+
+class Person(models.Model):
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    patronymic = models.CharField(max_length=40)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+
+    accepted = models.DateField()
+    salary = models.IntegerField()
+
+    def __str__(self):
+        return self.name, self.job
+
+class Leader(models.Model):
+    owner = models.OneToOneField(Person, on_delete=models.CASCADE, blank=True)
+    myself = models.OneToOneField(Person, on_delete=models.CASCADE)
+    slaves = models.ForeignKey(Person, on_delete=models.CASCADE, blank=True)
+
+
+
+
+
