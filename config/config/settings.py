7e1cee8a01fbf202f66f9ae0545f9035097034f3
BenitoSwaggolini <profie_player@mail.ru>
2022-10-29 15:59:02 +0500

    added media files, fixed models

--- config/config/settings.py
+++ config/config/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tree.apps.TreeConfig',
-'mptt',
+    'mptt',
 
 ]
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -83,7 +80,7 @@ DATABASES = {
     }
 }
 
-#PostgreSQL
+# PostgreSQL
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
@@ -113,7 +110,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -125,7 +121,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -135,3 +130,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- config/tree/models.py
+++ config/tree/models.py
@@ -1,27 +1,20 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
 
 
-class Job(models.Model):
-    name = models.CharField(50)
-    text = models.TextField(1000, blank=True)
+
 
 class Person(models.Model):
     name = models.CharField(max_length=40)
     surname = models.CharField(max_length=40)
     patronymic = models.CharField(max_length=40)
-    job = models.ForeignKey(Job, on_delete=models.CASCADE)
-
+    job = models.CharField(max_length=50)
     accepted = models.DateField()
-    salary = models.IntegerField()
-
+    salary = models.IntegerField(validators=MinValueValidator(0))
+    photo = models.ImageField(blank=True, upload_to='media/')
     def __str__(self):
         return self.name, self.job
-
-class Leader(models.Model):
-    owner = models.OneToOneField(Person, on_delete=models.CASCADE, blank=True)
-    myself = models.OneToOneField(Person, on_delete=models.CASCADE)
-    slaves = models.ForeignKey(Person, on_delete=models.CASCADE, blank=True)
 
 
 
